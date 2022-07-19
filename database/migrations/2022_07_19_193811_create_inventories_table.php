c09355136df4faa468d0a237d89cb05f2a2e3b96
smashikurrahman <61595320+smashikurrahman@users.noreply.github.com>
2022-07-20 02:01:50 +0600

    table created

---
+++ database/migrations/2022_07_19_193811_create_inventories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInventoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventories', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('date');
+            $table->integer('bill_no');
+            $table->integer('customer_id');
+            $table->integer('total_discount');
+            $table->integer('total_amount');
+            $table->integer('due_amount');
+            $table->integer('paid_amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventories');
+    }
+}
